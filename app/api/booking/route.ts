a0b547519fe793423ddcea9e9ca9eed8fe75830e
Inderjeet Thakur <93086869+sonu8826@users.noreply.github.com>
2026-03-17 12:14:14 +0530

    changes

--- app/api/booking/route.ts
+++ app/api/booking/route.ts
@@ -1,13 +1,24 @@
+export const dynamic = "force-dynamic";
+
 import { connectDB } from "@/src/lib/mongodb";
 import Booking from "@/src/models/Booking";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
-  await connectDB();
+  try {
+    await connectDB();
 
-  const body = await req.json();
+    const body = await req.json();
 
-  const booking = await Booking.create(body);
+    const booking = await Booking.create(body);
 
-  return NextResponse.json(booking);
+    return NextResponse.json(booking);
+
+  } catch (error) {
+    console.error("BOOKING ERROR:", error);
+    return NextResponse.json(
+      { error: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
