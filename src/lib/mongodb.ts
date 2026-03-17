5fc39fbc480e6aeda18ad4808c391d10e1150787
Inderjeet Thakur <93086869+sonu8826@users.noreply.github.com>
2026-03-17 08:00:31 +0530

    fix mongodb uri

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,6 +2,8 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
+console.log("ENV CHECK:", process.env.MONGODB_URI);
+
 if (!MONGODB_URI) {
   throw new Error("Please define MONGODB_URI in .env.local");
 }
